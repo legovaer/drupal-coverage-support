9969c5dec31e4e9a68bf7f92dad8debc29bb9d67
Levi Govaerts <levi.govaerts@capgemini.com>
2016-06-04 19:05:22 +0100

    Avoiding the config file to be gone

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-mv config.data lib/hooks/
-. lib/hooks/config.data
 . init-hooks.sh
+. lib/hooks/config.data
 
 echo "Mirroring your Drupal.org repository with the repository we have on GitHub..."
 git clone --bare $GIT_DRUPAL_REPOSITORY /tmp/sourcegit.git
